5f06371cd959c684f2d069aa8c2ce2f8c43c02f7
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-22 14:48:15 +0000

    Added in variables, and console logged to check I had correctly targeted my desired elements

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 import {morse} from "./translator.js";
 
-console.log(morse("."))
+// console.log(morse("."))
 
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
+const translateBtn = document.querySelector(".translateBtn");
+
+// console.log(input);
+// console.log(output);
+// console.log(translateBtn);
