eb3ac538c34c6397d7433261d5cbc065f8264bde
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-22 14:43:09 +0000

    created index.html and created a form to sort inputs and outputs. Created main.js

--- translator.js
+++ translator.js
@@ -1,13 +1,114 @@
-export const translate = (message) => {
-    if (message == "bonjour") {
-        return "hello";
-    } else if (message == "je m'appelle") {
-        return "my name is";
-    } else if (message == "oui") {
-        return "yes";
-    } else if (message == "non") {
-        return "no";
-    } else if (message == "au revoir") {
-        return "goodbye";
+// create a function to check every variable
+
+export const morse = (code) => {
+    if(code == ".-") {
+      return "a"
     }
-};
+  
+    else if(code == "-...") {
+      return "b"
+    }
+  
+    else if(code == "-.-.") {
+      return "c"
+    }
+  
+    else if(code == "-..") {
+      return "d"
+    }
+  
+    else if(code == ".") {
+      return "e"
+    }
+  
+    else if(code == "..-.") {
+      return "f"
+    }
+  
+    else if(code == "--.") {
+      return "g"
+    }
+  
+    else if(code == "....") {
+      return "h"
+    }
+  
+    else if(code == "..") {
+      return "i"
+    }
+  
+    else if(code == ".---") {
+      return "j"
+    }
+  
+    else if(code == "-.-") {
+      return "k"
+    }
+  
+    else if(code == ".-..") {
+      return "l"
+    }
+  
+    else if(code == "--") {
+      return "m"
+    }
+  
+    else if(code == "-.") {
+      return "n"
+    }
+  
+    else if(code == "---") {
+      return "o"
+    }
+  
+    else if(code == ".--.") {
+      return "p"
+    }
+  
+    else if(code == "--.-") {
+      return "q"
+    }
+  
+    else if(code == ".-.") {
+      return "r"
+    }
+  
+    else if(code == "...") {
+      return "s"
+    }
+  
+    else if(code == "-") {
+      return "t"
+    }
+  
+    else if(code == "..-") {
+      return "u"
+    }
+  
+    else if(code == "...-") {
+      return "v"
+    }
+  
+    else if(code == ".--") {
+      return "w"
+    }
+  
+    else if(code == "-..-") {
+      return "x"
+    }
+  
+    else if(code == "-.--") {
+      return "y"
+    }
+  
+    else if(code == "--..") {
+      return "z"
+    }
+  
+    else if(code == "/") {
+      return " "
+    }
+    else {
+      return "unavailable"
+    }
+  };
--- translator.test.js
+++ translator.test.js
@@ -1,43 +1,134 @@
-// Write some tests to translate...
-// French to English
 
-// const { exportAllDeclaration } = require("babel-types");
-
-
-import { translate } from './translator.js';
-import { it, expect } from '@jest/globals';
+import {morse} from "./translator";
+import {describe, it, expect} from "@jest/globals";
 
 
-// it should translate bonjour to hello (English to French)
-it ("translates bonjour to hello", () => {
-    // we need a function to test
-    const result = translate("bonjour");
-    // we need ot check what that function returns, is what we EXPECT
-    expect(result).toBe("hello");
-});
+// create tests for alphabet
 
-// it should translate je m'appelle to my name is (French to English)
-it ("translates je m'appelle to my name is", () => {
-    const result = translate("je m'appelle");
-    expect(result).toBe("my name is");
-});
+describe("translate single letters to morse", () => {
 
-// it should translate oui to yes
-it ("translates oui to yes", () => {
-    const result = translate("oui");
-    expect(result).toBe("yes");
-});
+    it ("should translate .- to a", () => {
+    const result = morse(".-");
+    expect(result).toBe("a");
+    })
 
-// it should translate non to no
-it ("translates non to no", () => {
-    const result = translate("non");
-    expect(result).toBe("no");
-});
+    it ("should translate -... to b", () => {
+    const result = morse("-...");
+    expect(result).toBe("b");
+    })
 
-// it should translate au revoir to goodbye
-it ("translates au revoir to goodbye", () => {
-    const result = translate("au revoir");
-    expect(result).toBe("goodbye");
-});
+    it ("should translate -.-. to c", () => {
+    const result = morse("-.-.");
+    expect(result).toBe("c");
+    })
 
+    it ("should translate -.. to d", () => {
+    const result = morse("-..");
+    expect(result).toBe("d");
+    })
 
+    it ("should translate . to e", () => {
+    const result = morse(".");
+    expect(result).toBe("e");
+    })
+    it ("should translate ..-. to f", () => {
+    const result = morse("..-.");
+    expect(result).toBe("f");
+    })
+
+    it ("should translate --. to g", () => {
+    const result = morse("--.");
+    expect(result).toBe("g");
+    })
+    it ("should translate .... to h", () => {
+    const result = morse("....");
+    expect(result).toBe("h");
+    })
+
+    it ("should translate .. to i", () => {
+    const result = morse("..");
+    expect(result).toBe("i");
+    })
+    it ("should translate .--- to j", () => {
+    const result = morse(".---");
+    expect(result).toBe("j");
+    })
+
+    it ("should translate -.- to k", () => {
+    const result = morse("-.-");
+    expect(result).toBe("k");
+    })
+    it ("should translate .-.. to l", () => {
+    const result = morse(".-..");
+    expect(result).toBe("l");
+    })
+
+    it ("should translate -- to m", () => {
+    const result = morse("--");
+    expect(result).toBe("m");
+    })
+    it ("should translate -. to n", () => {
+    const result = morse("-.");
+    expect(result).toBe("n");
+    })
+
+    it ("should translate --- to o", () => {
+    const result = morse("---");
+    expect(result).toBe("o");
+    })
+    it ("should translate .--. to p", () => {
+    const result = morse(".--.");
+    expect(result).toBe("p");
+    })
+
+    it ("should translate --.- to q", () => {
+    const result = morse("--.-");
+    expect(result).toBe("q");
+    })
+    it ("should translate .-. to r", () => {
+    const result = morse(".-.");
+    expect(result).toBe("r");
+    })
+
+    it ("should translate ... to s", () => {
+    const result = morse("...");
+    expect(result).toBe("s");
+    })
+    it ("should translate - to t", () => {
+    const result = morse("-");
+    expect(result).toBe("t");
+    })
+
+    it ("should translate ..- to u", () => {
+    const result = morse("..-");
+    expect(result).toBe("u");
+    })
+    it ("should translate ...- to v", () => {
+    const result = morse("...-");
+    expect(result).toBe("v");
+    })
+
+    it ("should translate .-- to w", () => {
+    const result = morse(".--");
+    expect(result).toBe("w");
+    })
+    it ("should translate -..- to x", () => {
+    const result = morse("-..-");
+    expect(result).toBe("x");
+    })
+
+    it ("should translate -.-- to y", () => {
+    const result = morse("-.--");
+    expect(result).toBe("y");
+    })
+    it ("should translate --.. to z", () => {
+    const result = morse("--..");
+    expect(result).toBe("z");
+    })
+
+    it ("should translate / to be a space", () => {
+    const result = morse("/");
+    expect(result).toBe(" ");
+    })
+
+})
